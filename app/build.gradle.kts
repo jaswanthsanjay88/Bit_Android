4a79672b2d94b9240d63a20949e20d4dbf68e556
Jaswanth Sanjay <jaswanthsanjay88@gmail.com>
2026-07-10 10:12:38 +0530

    Bump release version to 1.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bit"
         minSdk = 29
         targetSdk = 36
-        versionCode = 38
-        versionName = "1.6.1"
+        versionCode = 39
+        versionName = "1.6.2"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
