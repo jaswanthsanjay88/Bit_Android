b352a5d6107ddd24ae0b2977f2f7b4ae741db5d8
Jaswanth Sanjay <jaswanthsanjay88@gmail.com>
2026-08-08 08:27:28 +0530

    Bump version to 2.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.bit"
         minSdk = 29
         targetSdk = 36
-        versionCode = 74
-        versionName = "2.0.5"
+        versionCode = 75
+        versionName = "2.0.6"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
